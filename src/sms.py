ba1a969336b825191d7453e876923ce19efbee32
Victor <victormoselo567@gmail.com>
2022-11-01 15:54:27 +0300

    .

---
+++ src/sms.py
@@ -0,0 +1,16 @@
+# create a py class that has props of the api
+# init them and create a fun called send sms athat has a class props as parameters
+
+
+
+
+
+class SMS:
+
+    def message():
+        x = 45
+        return x
+
+
+
+    
