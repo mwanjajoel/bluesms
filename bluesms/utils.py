86d6405fd3a99bd42fbda66235a3338cda685009
Arthur Nangai <arthur.nangai@gmail.com>
2022-11-06 17:43:13 +0300

    chore(utils) - Remove unnecessary variable

--- bluesms/utils.py
+++ bluesms/utils.py
@@ -7,7 +7,6 @@ def validate_phone_number(phone_number) -> bool:
     For example +256703000000
     """
     try:
-        ans = re.match('^\+\d{1,3}\d{3,}$', phone_number)
-        return bool(ans)
+        return bool(re.match('^\+\d{1,3}\d{3,}$', phone_number))
     except ValueError:
         return False
